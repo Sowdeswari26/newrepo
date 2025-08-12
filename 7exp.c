c04688897239344da8e6910ec5a0bbb943248ca1
Sowdeswari26 <sowdeswari47@gmail.com>
2025-08-12 21:19:51 +0530

    7th

---
+++ 7exp.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("This is the first statement.\n");
+    printf("\n");  // This prints a blank line
+    printf("This is the second statement.\n");
+    return 0;
+}
