2b1b858a3aa890df0d57185de9ab472da840ff8d
Sowdeswari26 <sowdeswari47@gmail.com>
2025-08-13 05:18:59 +0530

    14th

---
+++ 13exp.c
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+    std::cout << "Enter two integers: ";
+    std::cin >> a >> b;
+
+    int sum = a + b;
+    std::cout << "Sum: " << sum << std::endl;
+
+    return 0;
+}
